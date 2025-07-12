67b01a000e4258e7fa0fec9fa4db922f175f5885
muhammad maaz <compiletimeterror@gmail.com>
2025-07-13 00:30:05 +0500

    commit

--- knexfile.js
+++ knexfile.js
@@ -5,12 +5,12 @@ export default {
   development: {
     client: "pg",
     connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false, // Required by Render
+      },
     },
+
     pool: { min: 2, max: 10 },
     migrations: {
       directory: "./src/db/migrations",
--- src/db.js
+++ src/db.js
@@ -1,16 +1,16 @@
 import knex from "knex";
 import dotenv from "dotenv";
+import config from "../knexfile.js";
 
 dotenv.config();
 
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DB_HOST,
-    port: Number(process.env.PG_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false, // Required by Render
+    },
   },
   pool: {
     min: 2,
