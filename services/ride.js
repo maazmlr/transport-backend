fd7b21f13404796ff4bf26846374109f010a814f
muhammad maaz <compiletimeterror@gmail.com>
2025-08-05 11:00:24 +0500

    ddd

--- services/ride.js
+++ services/ride.js
@@ -12,7 +12,7 @@ export const RideService = {
   async getUserRides(userId) {
     return await RideModel.findByUser(userId);
   },
-
+//hhh
   async getDriverRides(driver_id) {
     return await RideModel.findByDriver(driver_id);
   },
