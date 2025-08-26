8b646405b504e7214f4501051fb965c7e61f6bbf
Muhammad Maaz <compiletimeterror@gmail.com>
2025-08-26 22:56:23 +0500

    cjjd

---
+++ src/db/migrations/20250826175552_add_driver_verification_status.cjs
@@ -0,0 +1,22 @@
+// migrations/20250820120000_add_driver_verification_status.js
+module.exports = {
+  up: async function (knex) {
+    await knex.schema.alterTable("users", (table) => {
+      table
+        .enu("driver_verification_status", ["pending", "verified", "rejected"], {
+          useNative: true,
+          enumName: "driver_verification_enum", // for Postgres
+        })
+        .defaultTo("pending");
+    });
+  },
+
+  down: async function (knex) {
+    await knex.schema.alterTable("users", (table) => {
+      table.dropColumn("driver_verification_status");
+    });
+
+    // For Postgres: cleanup enum type
+    await knex.raw("DROP TYPE IF EXISTS driver_verification_enum");
+  },
+};
