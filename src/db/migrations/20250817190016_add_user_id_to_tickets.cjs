86d6fba6465d301a698738b00cb9c22cff224ead
Muhammad Maaz <compiletimeterror@gmail.com>
2025-08-26 23:07:36 +0500

    gi

--- src/db/migrations/20250817190016_add_user_id_to_tickets.cjs
+++ src/db/migrations/20250817190016_add_user_id_to_tickets.cjs
@@ -3,7 +3,7 @@
 exports.up = function (knex) {
   return knex.schema.table("tickets", function (table) {
     table
-      .integer("user_id")
+      .uuid("user_id")
       .unsigned()
       .references("id")
       .inTable("users")
